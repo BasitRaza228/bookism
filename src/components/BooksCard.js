20b2f7ee0fc192887874f3273c49dd69df16f017
Syed Basit Raza <basitraza228@gmail.com>
2021-02-04 00:25:57 +0500

    bookscard component inirialized

---
+++ src/components/BooksCard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Card from 'react-bootstrap/Card';
+
+export default class BooksCard extends Component {
+    contructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+
+                </div>
+            </div>
+        )
+    }
+}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,16 +6,18 @@ import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
+import  "../App.css";
 
 function NavBar() {
-    return (
-        <>
-  <Navbar bg="light" variant="light">
+  return (
+    <> 
+    <Navbar bg="light" variant="light">
     <Navbar.Brand href="#home">Bookism</Navbar.Brand>
     <Nav className="ml-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link className="Navbar-links active" href="#home">Home</Nav.Link>
+      <Nav.Link className="Navbar-links" href="#features">Gallery</Nav.Link>
+      <Nav.Link className="Navbar-links" href="#pricing">About</Nav.Link>
+      <Nav.Link className="Navbar-links" href="#pricing">Contact Us</Nav.Link>
     </Nav>
   </Navbar>
     </>
